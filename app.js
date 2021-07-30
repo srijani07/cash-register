1c7837fcc86337beba72184f75e9cc2eb8a9f0dc
srijani07 <srijaniparui07@gmail.com>
2021-07-30 18:12:46 +0530

    feat : display paid amount after click

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var billAmount = document.querySelector("#bill-amount");
 var paidAmount = document.querySelector("#paid-amount");
-var btnCheck = document.querySelector("#check-btn");
-var outputArea = document.querySelector("#output-area");
+var outputArea = document.querySelector(".output-area");
+var billCheck = document.querySelector("#enter-bill");
+var givenArea = document.querySelector("#given-amount");
+var btnCheck = document.querySelector("#check-btn")
+var paidArea = document.querySelector(".paid-area")
 
-var outputTable = document.querySelector("#output-table")
+
+
+function billHandler() { //checks the entered bill amount
+
+    if(parseInt(billAmount.value) <= 0 || billAmount.value === "")
+    outputArea.innerHTML = "Please enter appropriate input";
+
+    else
+    paidArea.style.display = "block";
+    billCheck.style.display = "none";
+}
+
 function clickHandler() {
-if(parseInt(billAmount.value) <=0 || parseInt(paidAmount.value) <= 0) {
+    var paidAmount = document.querySelector("#paid-amount");
+    
+
+    //checks if the input paid amount is at least 1 and is a number
+if(parseInt(paidAmount.value) <= 0 || paidAmount.value ==="") {
     outputArea.innerHTML = "Please enter appropriate input."
 }
+
 
 else if(parseInt(billAmount.value) === parseInt(paidAmount.value)) {
     outputArea.innerHTML = "No return needed."
@@ -20,9 +39,7 @@ else if(parseInt(billAmount.value) > parseInt(paidAmount.value)) {
 
 else {
 var change = parseInt(paidAmount.value)-parseInt(billAmount.value);
-outputArea.innerHTML="Total change : "+change+ "<br/>";
-console.log(change);
-var notes = [2000,1000,500,100,50,20,10,5,2,1];
+var notes = [2000,1000,500,200,100,50,20,10,5,2,1];
 var frequency = [];
 
 for(var i=0;i<notes.length;i++) {
@@ -30,20 +47,19 @@ frequency[i] = Math.floor(change/notes[i]);
 change=change%notes[i];
 }
 
+for(var i=0;i<frequency.length;i++) { 
+    if(frequency[i] === 0) {
+        frequency[i] = "";
+    }
+}
 
-    var output="";
-    for(var i=0;i<notes.length;i++) {
-        if(frequency[i] !== 0)
-        outputArea.innerHTML+="Rs."+notes[i]+" : "+frequency[i]+" note(s)<br/>";
-        }
-        console.log(output);
-    
+//creating the table
+outputArea.innerHTML = "<table><tr><td>Denomination</td><td>"+notes[0]+"</td><td>"+notes[1]+"</td><td>"+notes[2]+"</td><td>"+notes[3]+"</td><td>"+notes[4]+"</td><td>"+notes[5]+"</td><td>"+notes[6]+"</td><td>"+notes[7]+"</td><td>"+notes[8]+"</td><td>"+notes[9]+"</td><td>"+notes[10]+"</tr><tr>"+"<td>Number of notes </td><td>"+frequency[0]+"</td><td>"+frequency[1]+"</td><td>"+frequency[2]+"</td><td>"+frequency[3]+"</td><td>"+frequency[4]+"</td><td>"+frequency[5]+"</td><td>"+frequency[6]+"</td><td>"+frequency[7]+"</td><td>"+frequency[8]+"</td><td>"+frequency[9]+"</td><td>"+frequency[10]+"</td></tr></table><br/>";
 
 
 }
 }
 
+
+billCheck.addEventListener("click",billHandler);
 btnCheck.addEventListener("click",clickHandler);
-
-
-
